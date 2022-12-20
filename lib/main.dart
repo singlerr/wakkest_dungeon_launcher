58787402246865e629b439b72710ed7181aafe33
Singlerr <singlerr@soongsil.ac.kr>
2022-12-20 19:56:36 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+import 'dart:io' show Platform;
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:process_run/shell.dart';
+
+void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  runApp(DevicePreview(
+    enabled: false,
+    builder: (context) => const Launcher(),
+  ));
+}
+
+class Launcher extends StatelessWidget {
+  const Launcher({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wakest Dungeon Launcher',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const LauncherPage(title: 'Wakest Dungeon Launcher v1.0'),
+    );
+  }
+}
+
+class LauncherPage extends StatefulWidget {
+  const LauncherPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LauncherPage> createState() => _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  void launch() async {
+    String executable = "\"runtime\\windows\\bin\\java.exe\"";
+    if (Platform.isMacOS) {
+      executable = "./runtime/macosx/bin/java";
+    }
+    String script =
+        "$executable -jar ${Directory.current.path}\\app\\desktop-1.0.jar";
+    Shell().run(script, onProcess: (process) {
+      exit(1);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 500,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: launch,
+                child: const Text("게임 실행",
+                    style: TextStyle(
+                        fontFamily: 'NotoSansKR',
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold)),
+              ),
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
